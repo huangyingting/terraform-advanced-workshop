af462197bf6398477e3993e610936ebb9143d7fc
Yingting Huang <24954047+huangyingting@users.noreply.github.com>
2025-09-08 12:42:48 +0800

    Update code

--- solutions/lab4/main.tf
+++ solutions/lab4/main.tf
@@ -14,38 +14,6 @@ resource "azurerm_resource_group" "main" {
   })
 }
 
-# Storage Account for application data (not Terraform state)
-resource "azurerm_storage_account" "app_storage" {
-  name                     = "st${var.project_name}${var.environment}${random_id.suffix.hex}"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = var.environment == "production" ? "GRS" : "LRS"
-
-  # Security settings
-  allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = false
-  min_tls_version                 = "TLS1_2"
-
-  blob_properties {
-    versioning_enabled       = true
-    last_access_time_enabled = true
-
-    delete_retention_policy {
-      days = var.backup_retention_days
-    }
-
-    container_delete_retention_policy {
-      days = var.backup_retention_days
-    }
-  }
-
-  tags = merge(var.tags, {
-    Environment = var.environment
-    Purpose     = "application-storage"
-  })
-}
-
 # Virtual Network
 resource "azurerm_virtual_network" "main" {
   name                = "${var.project_name}-${var.environment}-vnet"
